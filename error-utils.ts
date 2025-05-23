859e70dcc17f619161715b20d25edf348b72a2ba
Audrey Ogonna. <audreyjezy@gmail.com>
2025-05-23 03:24:28 +0100

    chore: remove unused code

--- error-utils.ts
+++ error-utils.ts
@@ -1,7 +1,22 @@
-import { ERROR_CATEGORIES } from "./error.model";
+import { ERROR_CATEGORIES, LanguageCode } from "./error.model";
 import { ERROR_SYSTEM } from "./classification";
 import { AppErrors } from "./errors";
 
+/**
+ * Retrieves the error message for a given error code, in the specified language.
+ *
+ * @param code - The error code for which the message is being retrieved.
+ * @param language - The language in which the error message should be retrieved.
+ * @returns The error message for the given code and language, or undefined if the language is not supported.
+ * @throws {Error} If the error code is unknown.
+ */
+export function getErrorMessageByLanguage(code: string, language: LanguageCode = "en"): string | undefined {
+  const error = ERROR_SYSTEM[code];
+  if (!error) {
+    throw new Error(`Unknown error code: ${code}`);
+  }
+  return error.description[language] || error.description.en;
+}
 
 /**
  * A utility class for error operations such as type-checking and introspection.
--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 export * from "./error.model";
 export * from "./classification";
-export * from "./utils";
-export  * from "./errors";
-export  * from "./error-utils"
+export * from "./errors";
+export * from "./error-utils"
